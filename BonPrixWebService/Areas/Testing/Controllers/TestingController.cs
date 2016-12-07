fc6fa6cedf7dd17d6833c5b0a77a48c18780a983
hstd004 <hstd004@VDIW81PERS-7>
2016-12-07 15:51:12 +0000

    Commit after presentation, accepted method as being OK

---
+++ BonPrixWebService/Areas/Testing/Controllers/TestingController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BonPrixWebService.Areas.Testing.Controllers
+{
+    public class TestingController : Controller
+    {
+
+        public ActionResult XSDView()
+        {
+            ViewBag.fileSuccessList = "";
+
+            var fils = System.IO.Directory.EnumerateFiles(Server.MapPath("/xsd"));
+
+
+            List<String> fls = new List<String>();
+
+            foreach (var f in fils)
+            {
+               
+                fls.Add("/xsd/" + Path.GetFileName(f)  );
+
+            }
+
+
+            ViewBag.vb_fls = fls;
+
+            return View();
+
+        }
+
+        public ActionResult FileView()
+        {
+            ViewBag.fileSuccessList = "";
+
+            var fils = System.IO.Directory.EnumerateFiles(Server.MapPath("/xml/success"));
+
+
+            List<String> fls = new List<String>();
+            List<String> flf = new List<String>();
+            List<String> cdts = new List<String>();
+            List<String> cdtf = new List<String>();
+
+
+
+
+            foreach ( var f in fils )
+            {
+                DateTime cdt = System.IO.File.GetCreationTime(f);
+                string fnm = f.Substring(Server.MapPath("").Length-3);
+                fls.Add("/xml/success/" + Path.GetFileName(f)) ;
+                cdts.Add(cdt.ToString("yyyy-MM-dd hh:mm:ss"));
+
+            }
+
+            ViewBag.fileFailureList = "";
+
+            fils = System.IO.Directory.EnumerateFiles(Server.MapPath("/xml/failure"));
+
+            foreach (var f in fils)
+            {
+                DateTime cdt = System.IO.File.GetCreationTime(f);
+                string fnm = f.Substring(Server.MapPath("").Length - 3);
+                flf.Add("/xml/failure/" + Path.GetFileName(f));
+
+                cdtf.Add(cdt.ToString("yyyy-MM-dd hh:mm:ss"));
+            }
+
+            ViewBag.vb_fls = fls;
+            ViewBag.vb_cdts = cdts;
+            ViewBag.vb_flf = flf;
+            ViewBag.vb_cdtf = cdtf;
+
+
+            return View();
+        }
+        // GET: Testing/Testing
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Testing/Testing/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Testing/Testing/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Testing/Testing/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Testing/Testing/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Testing/Testing/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Testing/Testing/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Testing/Testing/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- BonPrixWebService/Controllers/OpenXMLController.cs
+++ BonPrixWebService/Controllers/OpenXMLController.cs
@@ -32,6 +32,319 @@ namespace BonPrixWebService.Controllers
         /// Accepts XML to process
         /// </summary>
 //        public Task<HttpResponseMessage> Post([FromBody]string value)
+
+
+        private Task <HttpResponseMessage> PostForm(string formdata)
+        {
+            string fdata = "";
+
+            fdata = formdata.Replace("thisXML=","");
+            fdata = fdata.Trim();
+
+            /*
+          * Note : the max size of content that can be posted is governed in the web.config file
+          * see entry for <httpRuntime targetFramework="4.5" maxRequestLength ="24576" />
+          * maxrequestlength is in kb.
+          *
+          * Note : IIS manager might also have a setting for the maxAllowedContentLength so check that too
+          * though in this caseit worked anyway
+          */
+
+
+            String xmlName = "";
+            String localFile = "";
+            HttpResponseMessage theResponse;
+
+            HttpStatusCode statusCodeBAD = HttpStatusCode.BadRequest;
+            HttpStatusCode statusCodeOK = HttpStatusCode.OK;
+            var infomsgs = new List<string>();
+            var errs = new List<string>();
+            XDocument rsp;
+
+            statusCodeBAD = statusCodeOK;
+            try
+            {
+
+                /*
+                 *  get the actual request XML as a string so we can see what request it is
+                 * 
+                 */
+
+                try
+                {
+                        if (fdata.Length == 0)
+                        {
+                            infomsgs.Clear();
+                            errs.Clear();
+                            errs.Add("Empty File Sent");
+
+                            rsp = createResponse("", xmlName, false, infomsgs, errs);
+
+                            theResponse = new HttpResponseMessage(statusCodeBAD);
+
+                            theResponse.Content = new StringContent(rsp.ToString());
+
+                            return Task.FromResult(theResponse);
+
+                        }
+                }
+                catch (Exception e)
+                {
+                    theResponse = new HttpResponseMessage(statusCodeBAD);
+                    errs.Clear();
+                    infomsgs.Clear();
+
+                    errs.Add(e.Message);
+
+                    rsp = createResponse("", xmlName, false, infomsgs, errs);
+
+                    theResponse.Content = new StringContent(rsp.ToString());
+
+                    try
+                    {
+                        localFile = System.Web.HttpContext.Current.Server.MapPath("/xml/failure/") + DateTime.Now.ToString("yyyyMMddHHmmss") + "_" + xmlName + "_non_xml.xml";
+                        System.IO.File.WriteAllText(localFile, fdata);
+                        
+                    }
+                    catch (Exception E)
+                    {
+
+                    }
+
+                    return Task.FromResult(theResponse);
+
+                }
+
+
+                // OK we have some XML , lets loose validate it by parsing it , this will 
+                // trap any tag type errors
+                XDocument theDocument;
+
+                string thisAdjustmentIndicator = "";
+                try
+                {
+                    theDocument = XDocument.Parse(fdata);
+
+
+                    foreach (XElement element in theDocument.Descendants("AdjustmentIndicator"))
+                    {
+                        thisAdjustmentIndicator = element.Value;
+                    }
+
+                    xmlName = theDocument.Root.Name.LocalName;
+                }
+                catch (Exception e)
+                {
+                    // Save the file in the Failure folder on the server
+
+                    localFile = System.Web.HttpContext.Current.Server.MapPath("/xml/failure/") + DateTime.Now.ToString("yyyyMMddHHmmss") + "_" + xmlName + ".xml";
+
+                    System.IO.File.WriteAllText(localFile, fdata);
+
+                    theResponse = new HttpResponseMessage(statusCodeBAD);
+
+                    errs.Clear();
+                    infomsgs.Clear();
+
+                    errs.Add(e.Message);
+                    infomsgs.Add("General Parse Error");
+
+                    rsp = createResponse("None", xmlName, false, infomsgs, errs);
+
+                    theResponse.Content = new StringContent(rsp.ToString());
+
+                    return Task.FromResult(theResponse);
+
+                }
+
+                if (String.IsNullOrEmpty(thisAdjustmentIndicator))
+                {
+                    thisAdjustmentIndicator = "C";
+                }
+
+                if (xmlName.Length > 0)
+                {
+                    /*
+                     * parse the name of the xml file , see if we have an XSD
+                    *  look for an XSD with this xmlName in the filename to validate against 
+                    * 
+                    */
+                    String schemaXSDFile = "";
+                    if (thisAdjustmentIndicator == "D")
+                        schemaXSDFile = System.Web.HttpContext.Current.Server.MapPath("/xsd/" + xmlName + "_Delete.xsd");
+                    else
+                        schemaXSDFile = System.Web.HttpContext.Current.Server.MapPath("/xsd/" + xmlName + "_Add.xsd");
+
+                    if (!File.Exists(schemaXSDFile))
+                    {
+                        // Default to the none appended version 
+                        schemaXSDFile = System.Web.HttpContext.Current.Server.MapPath("/xsd/" + xmlName + ".xsd");
+                    }
+
+                    if (File.Exists(schemaXSDFile))
+                    {
+                        XmlSchemaSet schemaSet = new XmlSchemaSet();
+
+                        schemaSet.Add(null, schemaXSDFile);
+                        XmlReaderSettings settings = new XmlReaderSettings();
+                        settings.ValidationType = ValidationType.Schema;
+                        settings.ValidationFlags |= XmlSchemaValidationFlags.ReportValidationWarnings;
+
+                        try
+                        {
+                            theDocument.Validate(schemaSet, (s2, e) => errs.Add(e.Message));
+                        }
+                        catch (Exception e)
+                        {
+                            // Save the file in the Failure folder on the server
+
+                            localFile = System.Web.HttpContext.Current.Server.MapPath("/xml/failure/") + DateTime.Now.ToString("yyyyMMddHHmmss") + "_" + xmlName + ".xml";
+                                System.IO.File.WriteAllText(localFile, fdata );
+
+                            theResponse = new HttpResponseMessage(statusCodeBAD);
+
+                            errs.Clear();
+                            infomsgs.Clear();
+
+                            errs.Add(e.Message);
+                            rsp = createResponse(Path.GetFileNameWithoutExtension(schemaXSDFile), xmlName, false, infomsgs, errs);
+
+                            theResponse.Content = new StringContent(rsp.ToString());
+
+                            return Task.FromResult(theResponse);
+
+                        }
+                        /*
+                         * If it failed (msgs.Count > 0 ) then write out the mesages back to the browser / client and return BadRequest
+                         * If it worked return OK response
+                         */
+                        if (errs.Count == 0)
+                        {
+
+                            // Save the file in the Success folder on the server
+
+                            localFile = System.Web.HttpContext.Current.Server.MapPath("/xml/success/") + DateTime.Now.ToString("yyyyMMddHHmmss") + "_" + xmlName + ".xml";
+                            System.IO.File.WriteAllText(localFile, fdata );
+                            theResponse = new HttpResponseMessage(statusCodeOK);
+
+                            infomsgs.Clear();
+
+                            rsp = createResponse(Path.GetFileNameWithoutExtension(schemaXSDFile), xmlName, true, infomsgs, errs);
+
+                            theResponse.Content = new StringContent(rsp.ToString());
+
+                            return Task.FromResult(theResponse);
+                        }
+                        else
+                        {
+
+                            // Save the file in the Failure folder on the server
+
+                            localFile = System.Web.HttpContext.Current.Server.MapPath("/xml/failure/") + DateTime.Now.ToString("yyyyMMddHHmmss") + "_" + xmlName + ".xml";
+                                System.IO.File.WriteAllText(localFile, fdata);
+
+                            infomsgs.Clear();
+                            rsp = createResponse(Path.GetFileNameWithoutExtension(schemaXSDFile), xmlName, false, infomsgs, errs);
+                            theResponse = new HttpResponseMessage(statusCodeBAD);
+                            theResponse.Content = new StringContent(rsp.ToString());
+
+
+                            return Task.FromResult(theResponse);
+                        }
+
+                    }
+                }
+
+                /*
+                 *  To be here we have parsed the XML but cannot find a matching XSD to validate against ,so we use a generic
+                 *  one purely to trap missing/mismatching end tags etc called "generic.xsd"
+                 * 
+                 */
+
+                String schemaXSDGFile = System.Web.HttpContext.Current.Server.MapPath("/xsd/generic.xsd");
+                if (File.Exists(schemaXSDGFile))
+                {
+                    XmlSchemaSet schemaSet = new XmlSchemaSet();
+
+                    schemaSet.Add(null, schemaXSDGFile);
+                    XmlReaderSettings settings = new XmlReaderSettings();
+                    settings.ValidationType = ValidationType.Schema;
+                    settings.ValidationFlags |= XmlSchemaValidationFlags.ReportValidationWarnings;
+
+                    try
+                    {
+                        errs.Clear();
+                        theDocument.Validate(schemaSet, (s2, e) => errs.Add(e.Message));
+
+                        if (errs.Count == 0)
+                        {
+                            /*
+                             * No errors found , so return the OK response and some arbitrary text to say it worked 
+                             * save the XML in the success folder
+                             */
+                            theResponse = new HttpResponseMessage(statusCodeOK);
+                            infomsgs.Clear();
+                            rsp = createResponse("Generic", xmlName, true, infomsgs, errs);
+
+                            theResponse.Content = new StringContent(rsp.ToString());
+
+                            localFile = System.Web.HttpContext.Current.Server.MapPath("/xml/success/") + DateTime.Now.ToString("yyyyMMddHHmmss") + "_" + xmlName + ".xml";
+
+                            System.IO.File.WriteAllText(localFile, fdata);
+
+
+                            return Task.FromResult(theResponse);
+                        }
+                        else
+                        {
+                            throw new Exception();
+                        }
+                    }
+                    catch (Exception E)
+                    {
+                        localFile = System.Web.HttpContext.Current.Server.MapPath("/xml/failure/") + DateTime.Now.ToString("yyyyMMddHHmmss") + "_" + xmlName + ".xml";
+                        System.IO.File.WriteAllText(localFile, fdata);
+                        theResponse = new HttpResponseMessage(statusCodeBAD);
+
+                        infomsgs.Clear();
+
+                        rsp = createResponse("Generic", xmlName, false, infomsgs, errs);
+
+                        theResponse.Content = new StringContent(rsp.ToString());
+
+                        return Task.FromResult(theResponse);
+
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+            }
+
+            /*
+             *  To be here means we don't have a specific or a generic xsd to load 
+             *  No option but to class it as successful and save to success folder
+             * 
+             */
+            localFile = System.Web.HttpContext.Current.Server.MapPath("/xml/success/") + DateTime.Now.ToString("yyyyMMddHHmmss") + "_" + xmlName + ".xml";
+            System.Web.HttpContext.Current.Request.SaveAs(localFile, false);
+            theResponse = new HttpResponseMessage(statusCodeOK);
+
+            infomsgs.Clear();
+            errs.Clear();
+
+            infomsgs.Add("No XSD Match to Validate XML");
+
+            rsp = createResponse("NONE", xmlName, true, infomsgs, errs);
+
+            theResponse.Content = new StringContent(rsp.ToString());
+
+            return Task.FromResult(theResponse);
+
+
+
+        }
         public Task<HttpResponseMessage> Post()
         {
 
@@ -55,6 +368,23 @@ namespace BonPrixWebService.Controllers
             var errs = new List<string>();
             XDocument rsp;
 
+
+            if (HttpContext.Current.Request.ContentLength > int.MaxValue)
+            {
+                infomsgs.Clear();
+                errs.Clear();
+                errs.Add("XML is too large to process");
+
+                rsp = createResponse("", xmlName, false, infomsgs, errs);
+
+                theResponse = new HttpResponseMessage(statusCodeBAD);
+
+                theResponse.Content = new StringContent(rsp.ToString());
+
+                return Task.FromResult(theResponse);
+
+            }
+
             statusCodeBAD = statusCodeOK;
             try
             {
@@ -70,7 +400,9 @@ namespace BonPrixWebService.Controllers
                     using (var reader = new StreamReader(HttpContext.Current.Request.InputStream))
                     {
                         reader.BaseStream.Seek(0, SeekOrigin.Begin);
-                        bodyText = reader.ReadToEnd(); 
+                        bodyText = reader.ReadToEnd();
+                        bodyText = bodyText.Replace("thisXML=", "").Trim() ;
+
                         if (bodyText.Length == 0)
                         {
                             infomsgs.Clear();
@@ -302,12 +634,15 @@ namespace BonPrixWebService.Controllers
 
                             localFile = System.Web.HttpContext.Current.Server.MapPath("/xml/success/") + DateTime.Now.ToString("yyyyMMddHHmmss") + "_" + xmlName + ".xml";
 
-                            using (var reader = new StreamReader(HttpContext.Current.Request.InputStream))
-                            {
-                                reader.BaseStream.Seek(0, SeekOrigin.Begin);
-                                System.IO.File.WriteAllText(localFile, reader.ReadToEnd());
+//                            using (var reader = new StreamReader(HttpContext.Current.Request.InputStream))
+//                            {
+//                                reader.BaseStream.Seek(0, SeekOrigin.Begin);
+//                                var httpdata = reader.ReadToEnd();
+                                 
+                                System.IO.File.WriteAllText(localFile,bodyText);
+//                              System.Web.HttpContext.Current.Request.SaveAs(localFile, false);
                                 //                        System.Web.HttpContext.Current.Request.SaveAs(localFile, false);
-                            }
+//                            }
 
 
                             return Task.FromResult(theResponse);
